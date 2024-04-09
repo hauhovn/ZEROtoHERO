86e2c1a7b905fd004f0471e0bcd856057ab379da
vhclc <2270125@mail.vnclc.vn>
2024-04-09 17:02:49 +0700

    deco - 1

--- index.js
+++
@@ -1,7 +0,0 @@
-const myH1 = document.getElementById("myH1");
-const myBtn = document.getElementById("myBtn");
-
-myBtn.onclick = function(){
-    console.log(`hello word`);
-    myH1.textContent=myH1.innerText + ` :hello`
-}
