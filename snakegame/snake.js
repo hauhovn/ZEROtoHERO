88e1b726ecaebc025b259a6dcd181070b756e533
vhclc <2270125@mail.vnclc.vn>
2024-04-09 16:23:25 +0700

    snake ok

--- snakegame/snake.js
+++ snakegame/snake.js
@@ -7,20 +7,53 @@ var snakeContext = backgroundCanvas.getContext('2d');
 var count = 0;
 var score = 0;
 
-var keyLog;
+//huong di ban dau
+var keyLog = "ArrowRight";
 
 var gameZone = {
     x: 0,
     y: 0,
-    height: 10,
-    width: 10,
+    height: 15,
+    width: 15,
     grid: 32
 }
 
+var level = [
+    {
+        speed: 2,
+        color: 'green'
+    },
+    {
+        speed: 3,
+        color: 'yellow'
+    },
+    {
+        speed: 4,
+        color: 'blue'
+    },
+    {
+        speed: 5,
+        color: 'red'
+    }
+]
+
+function getLevel(_score){
+    if(_score>10){
+        console.log(`level 3 ${_score}`);
+        return level[3]
+    }else if(_score>5){
+        console.log(`level 2`);
+        return level[2]
+    }else if(_score>2){
+        console.log(`level 1`);
+        return level[1]
+    }else 
+   { console.log(`level 0  ${_score}`);return level[0]}
+}
 
 var snake = {
-    x: 10,
-    y: 10,
+    x: 2,
+    y: 2,
     // snake velocity. moves one gameZone.grid length every frame in either the x or y direction
     dx: gameZone.grid,
     dy: 0,
@@ -38,59 +71,107 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//auto moving
+setInterval(function () {
+    snakeMoving(getNextStep(keyLog));
+}, 500/getLevel(score).speed)
+
+
+
 // listen to keyboard events to move the snake
 document.addEventListener('keydown', function (e) {
 
+    if (e.key == keyLog) return
     // if > dieu kien bo sung sau
     if (!keyLog) { keyLog = e.key }
     console.log(keyLog);
+    let newSnakeLocation = getNextStep(e.key);
+    snakeMoving(newSnakeLocation);
+    keyLog = e.key;
 
+});
+
+function snakeMoving(nextStep) {
+    if (nextStep.x == snake.cells[0].x && nextStep.y == snake.cells[0].y) {
+        console.log(`Press ERROR!`)
+    } else {
+        console.log(`Press OK!`)
+        //nextStep.color = getLevel(score).color;
+        snake.cells.unshift(nextStep);
+
+        //check eat food
+        if (isEating(food, snake.cells[0])) {
+            drawFood()
+        } else removeTail();
+
+        //check gameover
+        if (isGameOver(snake.cells)) {
+            resetGame();
+
+            drawFood()
+            alert(`Game over!`)
+        }
+
+        //rerender
+        drawSnake(snake.cells)
+
+    }
+}
+
+function getNextStep(_key) {
     // init new snake cell = head snake cell
     let newSnakeLocation = { ...snake.cells[0] };
 
     // left arrow key & update snake cell location
-    if (e.key == 'ArrowLeft' || e.key == 'a') {
+    if (_key == 'ArrowLeft' || _key == 'a') {
         if (keyLog == 'ArrowRight' || keyLog == 'd') {
-            console.log(`press key ${e.key} error`);
-        } else newSnakeLocation.x = snake.cells[0].x - gameZone.grid;
-    }
-    else if (e.key == 'ArrowUp' || e.key == 'w') {
-        if (keyLog == 'ArrowDown' || keyLog == 's') {
-            console.log(`press key ${e.key} error`);
-        } else newSnakeLocation.y = snake.cells[0].y - gameZone.grid;
-    }
-    else if (e.key == 'ArrowRight' || e.key == 'd') {
-        if (keyLog == 'ArrowLeft' || keyLog == 'a') {
-            console.log(`press key ${e.key} error`);
-        } else
-            newSnakeLocation.x = snake.cells[0].x + gameZone.grid;
-    }
-    else if (e.key == 'ArrowDown' || e.key == 's') {
-        if (keyLog == 'ArrowUp' || keyLog == 'w') {
-            console.log(`press key ${e.key} error`);
+            console.log(`press key ${_key} error`);
         } else {
-            newSnakeLocation.y = snake.cells[0].y + gameZone.grid;
+            if (snake.cells[0].x <= gameZone.x * gameZone.grid) {
+                newSnakeLocation.x = (gameZone.width - 1) * gameZone.grid;
+                newSnakeLocation.y = getRandomInt(gameZone.y, gameZone.height) * gameZone.grid;
+            } else
+                newSnakeLocation.x = snake.cells[0].x - gameZone.grid;
+        }
+    }
+    else if (_key == 'ArrowUp' || _key == 'w') {
+        if (keyLog == 'ArrowDown' || keyLog == 's') {
+            console.log(`press key ${_key} error`);
+        } else {
+            if (snake.cells[0].y <= gameZone.y * gameZone.grid) {
+                newSnakeLocation.x = getRandomInt(gameZone.x, gameZone.width) * gameZone.grid;
+                newSnakeLocation.y = (gameZone.height - 1) * gameZone.grid;
+            } else
+                newSnakeLocation.y = snake.cells[0].y - gameZone.grid;
+        }
+    }
+    else if (_key == 'ArrowRight' || _key == 'd') {
+        if (keyLog == 'ArrowLeft' || keyLog == 'a') {
+            console.log(`press key ${_key} error`);
+        } else {
+            if (snake.cells[0].x >= (gameZone.width - 1) * gameZone.grid) {
+                newSnakeLocation.x = 0 * gameZone.grid;
+                newSnakeLocation.y = getRandomInt(gameZone.y, gameZone.height) * gameZone.grid;
+            } else
+                newSnakeLocation.x = snake.cells[0].x + gameZone.grid;
         }
 
     }
-
-    //final snake cell
-    //add cell to snake cells
-    if (newSnakeLocation.x == snake.cells[0].x && newSnakeLocation.y == snake.cells[0].y) {
-        console.log(`Press ERROR!`)
-    } else {
-        ; newSnakeLocation.color = randomColorCode();
-        snake.cells.unshift(newSnakeLocation);
-        keyLog = e.key;
+    else if (_key == 'ArrowDown' || _key == 's') {
+        if (keyLog == 'ArrowUp' || keyLog == 'w') {
+            console.log(`press key ${_key} error`);
+        } else {
+            if (snake.cells[0].y >= (gameZone.height - 1) * gameZone.grid) {
+                newSnakeLocation.x = getRandomInt(gameZone.x, gameZone.width) * gameZone.grid;
+                newSnakeLocation.y = 0;
+                console.log(`newSnakeLocation`, newSnakeLocation);
+            } else
+                newSnakeLocation.y = snake.cells[0].y + gameZone.grid;
+        }
 
     }
-
-    //rerender
-    if (isEating(food, snake.cells[0])) {
-        drawFood()
-    } else removeTail();
-    drawSnake(snake.cells)
-});
+    return newSnakeLocation;
+}
 
 
 function gamePlay() {
@@ -121,7 +202,7 @@ function drawBackground(height, width) {
 
     }
     // draw snake one cell at a time
-    backgroundContext.fillStyle = '#727271';
+    backgroundContext.fillStyle = '#000';
 
     //console.log(`snake.cells ${snake.cells.length}`)
     bg.forEach(function (cell, index) {
@@ -138,14 +219,9 @@ function drawSnake(snakeCells = []) {
     if (snakeCells.length == 0) {
         // khoi tao
         snakeCells = [
-            { x: gameZone.grid * 6, y: gameZone.grid * 4, count: count++, color: randomColorCode() },
-            { x: gameZone.grid * 5, y: gameZone.grid * 4, count: count++, color: randomColorCode() },
-            { x: gameZone.grid * 4, y: gameZone.grid * 4, count: count++, color: randomColorCode() }
-            // ,
-            // { x: gameZone.grid * 3, y: gameZone.grid * 4, count: count++, color: randomColorCode() },
-            // { x: gameZone.grid * 2, y: gameZone.grid * 4, count: count++, color: randomColorCode() },
-            // { x: gameZone.grid * 1, y: gameZone.grid * 4, count: count++, color: randomColorCode() },
-            // { x: gameZone.grid * 0, y: gameZone.grid * 4, count: count++, color: randomColorCode() }
+            { x: gameZone.grid * 3, y: gameZone.grid * 1, count: count++, color: getLevel(score).color },
+            { x: gameZone.grid * 2, y: gameZone.grid * 1, count: count++, color: getLevel(score).color },
+            { x: gameZone.grid * 1, y: gameZone.grid * 1, count: count++, color: getLevel(score).color }
         ]
         console.log(`init snake:`, snakeCells);
         snake.cells = snakeCells;
@@ -155,7 +231,7 @@ function drawSnake(snakeCells = []) {
     let size = maxSize;
     let upSize = (maxSize - minSize) / snake.cells.length;
 
-    snake.cells.forEach(function (cell, index) {
+    snakeCells.forEach(function (cell, index) {
 
         //remove old cell
         snakeContext.clearRect(cell.x, cell.y, gameZone.grid, gameZone.grid);
@@ -163,27 +239,66 @@ function drawSnake(snakeCells = []) {
         snakeContext.beginPath();
         snakeContext.arc(cell.x + gameZone.grid / 2, cell.y + gameZone.grid / 2, size, 0, 2 * Math.PI);
         size -= upSize;
-        snakeContext.fillStyle = cell.color;
+        snakeContext.fillStyle = getLevel(score).color;
         snakeContext.fill();
     }
 
     );
 }
 
-function drawFood() {
-    let foodX = getRandomInt(1, gameZone.height - 1) * gameZone.grid;
-    let foodY = getRandomInt(1, gameZone.width - 1) * gameZone.grid;
+function ClearSnake(snakeCells = []) {
 
-    snake.cells.forEach(cell => {
-        if (cell.x == foodX && cell.y == foodX) drawFood()
-    })
+    snake.cells.forEach(function (cell, index) {
 
-    console.log(`check eat x:${foodX} y:${foodY}`);
-    food.x = foodX, food.y = foodY
-    snakeContext.fillStyle = randomColorCode();
-    snakeContext.fillRect(foodX, foodY, gameZone.grid, gameZone.grid);
+        //remove old cell
+        snakeContext.clearRect(cell.x, cell.y, gameZone.grid, gameZone.grid);
+    }
+    );
 }
 
+async function drawFood() {
+    console.log(`drawFood()`);
+    snakeContext.fillStyle = randomColorCode();
+    food = await getFoodLocation(snake.cells, gameZone)
+    snakeContext.fillRect(food.x, food.y, gameZone.grid, gameZone.grid);
+}
+
+function getFoodLocation(snakeCells = [], _game) {
+    console.log(`getFoodLocation()`, snakeCells, _game);
+    let foodX, foodY, isOk = false, findCount = 0;
+
+    while (!isOk) {
+        isOk = true;
+        foodX = getRandomInt(0, _game.height) * _game.grid;
+        foodY = getRandomInt(0, _game.width) * _game.grid;
+        snakeCells.forEach(cell => {
+            if (cell.x == foodX && cell.y == foodY) {
+                console.log(`refind`);
+                isOk = false;
+            }
+        })
+        findCount++;
+        if (findCount == snakeCells.length - 1) {
+            isOk = false;
+            //reset game
+            resetGame();
+            alert(`You are winner`);
+            break;
+        }
+    }
+
+    console.log(` getFoodLocation check eat x:${foodX} y:${foodY}`);
+    return { x: foodX, y: foodY }
+}
+function resetGame() {
+    snakeContext.fillStyle = '#000';
+    snakeContext.fillRect(gameZone.x, gameZone.y, gameZone.width * gameZone.grid, gameZone.height * gameZone.grid)
+    snake.cells = snake.cells.slice(0, 3);
+    console.log(snake.cells);
+    drawSnake(snake.cells)
+    score = 0;
+    document.getElementById('score').textContent = score;
+}
 function removeTail() {
 
     let tail = snake.cells[snake.cells.length - 1];
@@ -197,132 +312,7 @@ function removeTail() {
 
 function randomColorCode() {
     let colors = [
-        `AliceBlue`,
-        `AntiqueWhite`,
-        `Aqua`,
-        `Aquamarine`,
-        `Azure`,
-        `Beige`,
-        `Bisque`,
-        `BlanchedAlmond`,
-        `Blue`,
-        `BlueViolet`,
-        `Brown`,
-        `BurlyWood`,
-        `CadetBlue`,
-        `Chartreuse`,
-        `Chocolate`,
-        `Coral`,
-        `CornflowerBlue`,
-        `Cornsilk`,
-        `Crimson`,
-        `DarkViolet`,
-        `DeepPink`,
-        `DeepSkyBlue`,
-        `DimGray`,
-        `DimGrey`,
-        `DodgerBlue`,
-        `FireBrick`,
-        `FloralWhite`,
-        `ForestGreen`,
-        `Fuchsia`,
-        `Gainsboro`,
-        `GhostWhite`,
-        `Gold`,
-        `GoldenRod`,
-        `Grey`,
-        `Green`,
-        `GreenYellow`,
-        `HoneyDew`,
-        `HotPink`,
-        `IndianRed`,
-        `Indigo`,
-        `Ivory`,
-        `Khaki`,
-        `Lavender`,
-        `LavenderBlush`,
-        `LawnGreen`,
-        `LemonChiffon`,
-        `LightBlue`,
-        `LightCoral`,
-        `LightCyan`,
-        `LightGoldenRodYellow`,
-        `LightGray`,
-        `LightGrey`,
-        `LightGreen`,
-        `LightPink`,
-        `LightSalmon`,
-        `LightSeaGreen`,
-        `LightSkyBlue`,
-        `LightSlateGray`,
-        `LightSlateGrey`,
-        `LightSteelBlue`,
-        `LightYellow`,
-        `Lime`,
-        `LimeGreen`,
-        `Linen`,
-        `Magenta`,
-        `Maroon`,
-        `MediumAquaMarine`,
-        `MediumBlue`,
-        `MediumOrchid`,
-        `MediumPurple`,
-        `MediumSeaGreen`,
-        `MediumSlateBlue`,
-        `MediumSpringGreen`,
-        `MediumTurquoise`,
-        `MediumVioletRed`,
-        `MidnightBlue`,
-        `MintCream`,
-        `MistyRose`,
-        `Moccasin`,
-        `NavajoWhite`,
-        `Navy`,
-        `OldLace`,
-        `Olive`,
-        `OliveDrab`,
-        `Orange`,
-        `OrangeRed`,
-        `Orchid`,
-        `PaleGoldenRod`,
-        `PaleGreen`,
-        `PaleTurquoise`,
-        `PaleVioletRed`,
-        `PapayaWhip`,
-        `PeachPuff`,
-        `Peru`,
-        `Pink`,
-        `Plum`,
-        `PowderBlue`,
-        `Purple`,
-        `Red`,
-        `RosyBrown`,
-        `RoyalBlue`,
-        `SaddleBrown`,
-        `Salmon`,
-        `SandyBrown`,
-        `SeaGreen`,
-        `SeaShell`,
-        `Sienna`,
-        `Silver`,
-        `SkyBlue`,
-        `SlateBlue`,
-        `SlateGray`,
-        `SlateGrey`,
-        `Snow`,
-        `SpringGreen`,
-        `SteelBlue`,
-        `Tan`,
-        `Teal`,
-        `Thistle`,
-        `Tomato`,
-        `Turquoise`,
-        `Violet`,
-        `Wheat`,
-        `White`,
-        `WhiteSmoke`,
-        `Yellow`,
-        `YellowGreen`,
+        "Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "
     ]
     return colors[Math.floor(Math.random() * colors.length)];
 }
@@ -362,4 +352,16 @@ function isEating(_food, snakeHead) {
         return true;
     }
     return false
+}
+
+function isGameOver(snakeCells, _game) {
+    // tu can than ran           
+    for (let i = 3; i < snakeCells.length; i++) {
+        console.log(`isGameOver `, snakeCells[0], snakeCells[i]);
+        if (snakeCells[0].x == snakeCells[i].x && snakeCells[0].y == snakeCells[i].y) {
+            return true;
+        }
+    }
+    return false;
+    //vat can
 }
