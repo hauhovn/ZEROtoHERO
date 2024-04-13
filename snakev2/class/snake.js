18031e340d237aa9b395fccfc431793a7304687c
Van Hau <hauhovnn@gmail.com>
2024-04-14 00:44:48 +0700

    snake game OK

--- snakev2/class/snake.js
+++ snakev2/class/snake.js
@@ -2,121 +2,124 @@ import { DIRECTION } from "../text-constants.js";
 import Cell from "./cell.js";
 
 export default class Snake {
-  constructor(cells, length = 3, direction = DIRECTION.RIGHT) {
-    this.cells = cells;
-    this.length = length;
-    this.direction = direction;
-    // document.addEventListener("keydown", (e) => this.keydown(e));
-    this.keydown = this.keydown.bind(this);
-    document.addEventListener("keydown", this.keydown);
-  }
-  set cells(cells){
-    this._cells=cells;
-  }
-  draw(ctx) {
-    
-    if(this.cells)this.cells.forEach((cell) => {
-      cell.draw(ctx);
-    });
-  }
-  move(ctx) {
-    if(!this.cells)return;
-    // add new head
-    const nextSteep = this.getNextStep();
-    if (nextSteep == null) console.log(nextSteep);
-    this.cells.unshift(nextSteep);
-    // draw snake
-    this.draw(ctx);
-    //remove teil
-    this.cells.length >= this.length +1 && this.removeTeil(ctx);
-  }
-  isDie(){
-    if(!this.cells)return;
-    if (this.cells.length < 4) return false;
-    const head = this.cells[0]; // Phần tử đầu tiên của rắn
-    for (let i = 1; i < this.cells.length; i++) {
-        if (head.x === this.cells[i].x && head.y === this.cells[i].y) {
-            return true; // Nếu đầu của rắn trùng với bất kỳ phần tử nào khác, rắn đã chết
+    constructor(_cells = [], _length = 3, _direction = DIRECTION.RIGHT) {
+        this.cells = _cells;
+        this.length = _length;
+        this.direction = _direction;
+        // document.addEventListener("keydown", (e) => this.keydown(e));
+        this.keydown = this.keydown.bind(this);
+        document.addEventListener("keydown", this.keydown);
+    }
+    set cells(_cells) {
+        this._cells = _cells;
+    }
+
+    get cells() {
+        return this._cells;
+    }
+    draw(ctx) {
+        if (this.cells || this.cells.length > 0) this.cells.forEach((cell) => {
+            cell.draw(ctx);
+        });
+    }
+    move(ctx) {
+        if (!this.cells) return;
+        // add new head
+        const nextSteep = this.getNextStep();
+        if (nextSteep == null) console.log(nextSteep);
+        this.cells.unshift(nextSteep);
+        // draw snake
+        this.draw(ctx);
+        //remove teil
+        this.cells.length >= this.length + 1 && this.removeTeil(ctx);
+    }
+    isDie() {
+        if (!this.cells) return;
+        if (this.cells.length < 4) return false;
+        const head = this.cells[0]; // Phần tử đầu tiên của rắn
+        for (let i = 1; i < this.cells.length; i++) {
+            if (head.x === this.cells[i].x && head.y === this.cells[i].y) {
+                return true; // Nếu đầu của rắn trùng với bất kỳ phần tử nào khác, rắn đã chết
+            }
         }
+        return false;
     }
-    return false;
-  }
 
-  removeTeil(ctx) {
-    this.cells[this.cells.length - 1].clear(ctx);
-    this.cells.pop();
-  }
-  grow(){
-    const head = Cell.clone(this.cells[0]);
-    this.cells.unshift(head);
-  }
-  eat(food) {
-    if (!this.cells) return false;
-      if (this.cells[0].x == food.x && this.cells[0].y == food.y) {
-        return true;
-      }
-    return false;
-  }
+    removeTeil(ctx) {
+        this.cells[this.cells.length - 1].clear(ctx);
+        this.cells.pop();
+    }
+    grow() {
+        const head = Cell.clone(this.cells[0]);
+        this.cells.unshift(head);
+    }
+    eat(food) {
+        if (!this.cells) return false;
+        if (this.cells[0].x == food.x && this.cells[0].y == food.y) {
+            return true;
+        }
+        return false;
+    }
 
-  getNextStep() {
-    if(!this.cells)return;
-    const newCell = Cell.clone(this.cells[0]);
-    // console.log(this.direction);
-    // get new location
-    if (this.direction === DIRECTION.UP) {
-      newCell.y -= newCell.height;
+    getNextStep() {
+        if (!this.cells) return;
+        const newCell = Cell.clone(this.cells[0]);
+        // console.log(this.direction);
+        // get new location
+        if (this.direction === DIRECTION.UP) {
+            newCell.y -= newCell.height;
+        }
+        if (this.direction === DIRECTION.DOWN) {
+            newCell.y += newCell.height;
+        }
+        if (this.direction === DIRECTION.RIGHT) {
+            newCell.x += newCell.width;
+        }
+        if (this.direction === DIRECTION.LEFT) {
+            newCell.x -= newCell.width;
+        }
+        if (newCell == this.cells[0] || newCell == this.cells[1]) return null;
+        // newCell.color = `#` + Math.floor(Math.random() * 16777215).toString(16);
+        return newCell;
     }
-    if (this.direction === DIRECTION.DOWN) {
-      newCell.y += newCell.height;
-    }
-    if (this.direction === DIRECTION.RIGHT) {
-      newCell.x += newCell.width;
-    }
-    if (this.direction === DIRECTION.LEFT) {
-      newCell.x -= newCell.width;
-    }
-    if (newCell == this.cells[0] || newCell==this.cells[1]) return null;
-    // newCell.color = `#` + Math.floor(Math.random() * 16777215).toString(16);
-    return newCell;
-  }
 
-  keydown = (e) => {
-    //Event Listener
-    if (
-      DIRECTION.UP.includes(e.code) &&
-      DIRECTION.UP !== this.direction &&
-      DIRECTION.DOWN !== this.direction
-    ) {
-      // key up
-      this.direction = DIRECTION.UP;
-      // console.log(this.direction);
-    }
-    if (
-      DIRECTION.DOWN.includes(e.code) &&
-      DIRECTION.DOWN !== this.direction &&
-      DIRECTION.UP !== this.direction
-    ) {
-      // key down
-      this.direction = DIRECTION.DOWN;
-      // console.log(this.direction);
-    }
-    if (
-      DIRECTION.RIGHT.includes(e.code) &&
-      DIRECTION.RIGHT !== this.direction &&
-      DIRECTION.LEFT !== this.direction
-    ) {
-      // key right
-      this.direction = DIRECTION.RIGHT;
-      // console.log(this.direction);
-    }
-    if (
-      DIRECTION.LEFT.includes(e.code) &&
-      DIRECTION.LEFT !== this.direction &&
-      DIRECTION.RIGHT !== this.direction
-    ) {
-      //key left
-      this.direction = DIRECTION.LEFT;
-      // console.log(this.direction);
-    }
-  };
+    keydown = (e) => {
+        //Event Listener
+        if (
+            DIRECTION.UP.includes(e.code) &&
+            DIRECTION.UP !== this.direction &&
+            DIRECTION.DOWN !== this.direction
+        ) {
+            // key up
+            this.direction = DIRECTION.UP;
+            // console.log(this.direction);
+        }
+        if (
+            DIRECTION.DOWN.includes(e.code) &&
+            DIRECTION.DOWN !== this.direction &&
+            DIRECTION.UP !== this.direction
+        ) {
+            // key down
+            this.direction = DIRECTION.DOWN;
+            // console.log(this.direction);
+        }
+        if (
+            DIRECTION.RIGHT.includes(e.code) &&
+            DIRECTION.RIGHT !== this.direction &&
+            DIRECTION.LEFT !== this.direction
+        ) {
+            // key right
+            this.direction = DIRECTION.RIGHT;
+            // console.log(this.direction);
+        }
+        if (
+            DIRECTION.LEFT.includes(e.code) &&
+            DIRECTION.LEFT !== this.direction &&
+            DIRECTION.RIGHT !== this.direction
+        ) {
+            //key left
+            this.direction = DIRECTION.LEFT;
+            // console.log(this.direction);
+        }
+    };
 }
--- snakev2/index.js
+++ snakev2/index.js
@@ -3,19 +3,23 @@ import Snake from "./class/snake.js";
 import { THEME_COLORS } from "./constants.js";
 
 const score = document.getElementById("score");
+const hightestScore = document.getElementById("hightest-score");
+const scoreHistory = document.getElementById("score-history");
+
+
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
 
 const GAME_SETTING = {
-  WIDTH: 400,
-  VERTI_CELL_NUM: 10,
-  HEIGHT: 400,
-  HORI_CELL_NUM: 10,
-  CELL_WIDTH: 40,
-  SPACE: 2,
-  WALL_WIDTH: 5,
-  WALL_COLOR: THEME_COLORS.DARK,
-  WALL_STYLE: "solid",
+    WIDTH: 400, //set here!
+    VERTI_CELL_NUM: 10, //set here!
+    HEIGHT: 400, //set here!
+    HORI_CELL_NUM: 10, //set here!
+    CELL_WIDTH: 40, //set here!
+    SPACE: 2,
+    WALL_WIDTH: 5, //set here!
+    WALL_COLOR: THEME_COLORS.DARK,
+    WALL_STYLE: "solid",
 };
 
 // WALL SETTING
@@ -24,141 +28,185 @@ canvas.width = GAME_SETTING.WIDTH;
 canvas.style.border = `${GAME_SETTING.WALL_WIDTH}px ${GAME_SETTING.WALL_STYLE} ${GAME_SETTING.WALL_COLOR}`;
 
 const initCells2 = [
-  new Cell(
-    120,
-    40,
-    GAME_SETTING.CELL_WIDTH,
-    GAME_SETTING.CELL_WIDTH,
-    THEME_COLORS.LIGHT
-  ),
-  new Cell(80, 40, GAME_SETTING.CELL_WIDTH, GAME_SETTING.CELL_WIDTH),
-  new Cell(40, 40, GAME_SETTING.CELL_WIDTH, GAME_SETTING.CELL_WIDTH),
+    new Cell(
+        120,
+        40,
+        GAME_SETTING.CELL_WIDTH,
+        GAME_SETTING.CELL_WIDTH,
+        THEME_COLORS.LIGHT
+    ),
+    new Cell(80, 40, GAME_SETTING.CELL_WIDTH, GAME_SETTING.CELL_WIDTH),
+    new Cell(40, 40, GAME_SETTING.CELL_WIDTH, GAME_SETTING.CELL_WIDTH),
 ];
 
-const snake = new Snake(initCells2, initCells2.length);
+const snake = new Snake([...initCells2], initCells2.length);
 var food = undefined;
 var mScore = 0;
+var history = [];
 
 // MAIN FUNCTION
 function gameLoop() {
-  //draw snake
-  snake.draw(context);
-  //frist spawn food
-  if (food == undefined) {
-    food = new Cell(
-      getRandomCell().x,
-      getRandomCell().y,
-      GAME_SETTING.CELL_WIDTH,
-      GAME_SETTING.CELL_WIDTH,
-      'yellow'
-    );
-    food.draw(context);
-  }
-  // check walls add next steep to cells
-  const next = isSnakeCollideWalls();
-  if (next.isChanged) {
-    snake.cells.unshift(next.value);
+    //draw snake
     snake.draw(context);
-    snake.removeTeil(context);
-  } else {
-    //move
-    snake.move(context);
-  }
-   //check die
-   if (snake.isDie()) {
-    //aler end game
-    alert(`Your score: ${mScore}`);
-    mScore=0;
-    score.textContent=mScore;
-    //reset game
-    context.clearRect(0,0,GAME_SETTING.WIDTH,GAME_SETTING.HEIGHT)
-    console.log(snake.cells.length);
-    snake.cells =initCells2;
-    console.log(snake.cells.length);
-    snake.draw(context)
-    spawnFood(context);
-  }
+    //frist spawn food
+    if (food == undefined) {
+        food = new Cell(
+            getRandomCell().x,
+            getRandomCell().y,
+            GAME_SETTING.CELL_WIDTH,
+            GAME_SETTING.CELL_WIDTH,
+            'yellow'
+        );
+        food.draw(context);
+    }
+    // check walls add next steep to cells
+    const next = isSnakeCollideWalls();
+    if (next.isChanged) {
+        snake._cells.unshift(next.value);
+        snake.draw(context);
+        snake.removeTeil(context);
+    } else {
+        //move
+        snake.move(context);
+    }
+    //check die
+    if (snake.isDie()) {
+        //Reset game
+        resetGame();
+    }
 
-  // check snake eat food
-  if (snake.eat(food)) {
-    snake.grow();
-    spawnFood(context);
+    // check snake eat food
+    if (snake.eat(food)) {
+        snake.grow();
+        spawnFood(context);
 
-    mScore++;
-    score.textContent = mScore;
-  }
+        mScore++;
+        score.textContent = mScore;
+    }
 
-  setTimeout(gameLoop, 1000 / 6);
+    setTimeout(gameLoop, 1000 / 6 / (mScore == 0 ? 1 : mScore / 2));
 }
 gameLoop();
 // setInterval(gameLoop, 1000 / 6 / snake.speed)
 
 function initCells() {
-  let rs = [];
-  let firstX = Math.floor(
-    Math.random() * GAME_SETTING.WIDTH * 0.5 - GAME_SETTING.CELL_WIDTH * 3
-  );
-  let firstY = Math.floor(Math.random() * GAME_SETTING.HEIGHT * 0.5);
-  console.log(`x:${firstX} y:${firstY}`);
-  for (let i = 0; i < 3; i++) {
-    rs.push(
-      new Cell(firstX, firstY, GAME_SETTING.CELL_WIDTH, GAME_SETTING.CELL_WIDTH)
+    let rs = [];
+    let firstX = Math.floor(
+        Math.random() * GAME_SETTING.WIDTH * 0.5 - GAME_SETTING.CELL_WIDTH * 3
     );
-    firstX -= GAME_SETTING.CELL_WIDTH;
-  }
-  return rs;
+    let firstY = Math.floor(Math.random() * GAME_SETTING.HEIGHT * 0.5);
+    console.log(`x:${firstX} y:${firstY}`);
+    for (let i = 0; i < 3; i++) {
+        rs.push(
+            new Cell(firstX, firstY, GAME_SETTING.CELL_WIDTH, GAME_SETTING.CELL_WIDTH)
+        );
+        firstX -= GAME_SETTING.CELL_WIDTH;
+    }
+    return rs;
+}
+function resetGame() {
+    // Alert the end of the game
+    alert(`Your score: ${mScore}`);
+    //Save history
+    history.unshift({ score: mScore, time: getCurrentTime() });
+
+    //Set hight score
+    hightestScore.textContent =
+        history.reduce((max, obj) => (obj.score > max.score ? obj : max), history[0]).score;
+
+    renderHistoryList();
+
+    // Reset the score
+    mScore = 0;
+    score.textContent = mScore;
+
+    // Clear the canvas
+    context.clearRect(0, 0, GAME_SETTING.WIDTH, GAME_SETTING.HEIGHT)
+
+    // Reset the snake to its initial position
+    snake.cells = [...initCells2];
+    console.log(snake);
+    snake.draw(context);
+
+    //WINNER
+
+
+    // Spawn a new food
+    spawnFood(context);
 }
 
 function isSnakeCollideWalls() {
-  const nextSteep = snake.getNextStep();
-  let isChanged = false;
-  if(!nextSteep)return ({isChanged});
-  if (nextSteep.x < 0) {
-    nextSteep.x = GAME_SETTING.WIDTH - GAME_SETTING.CELL_WIDTH;
-    isChanged = true;
-  }
-  if (nextSteep.x >= GAME_SETTING.WIDTH) {
-    nextSteep.x = 0;
-    isChanged = true;
-  }
-  if (nextSteep.y >= GAME_SETTING.HEIGHT) {
-    nextSteep.y = 0;
-    isChanged = true;
-  }
-  if (nextSteep.y < 0) {
-    nextSteep.y = GAME_SETTING.HEIGHT - GAME_SETTING.CELL_WIDTH;
-    isChanged = true;
-  }
-  return { isChanged, value: nextSteep };
+    const nextSteep = snake.getNextStep();
+    let isChanged = false;
+    if (!nextSteep) return ({ isChanged });
+    if (nextSteep.x < 0) {
+        nextSteep.x = GAME_SETTING.WIDTH - GAME_SETTING.CELL_WIDTH;
+        isChanged = true;
+    }
+    if (nextSteep.x >= GAME_SETTING.WIDTH) {
+        nextSteep.x = 0;
+        isChanged = true;
+    }
+    if (nextSteep.y >= GAME_SETTING.HEIGHT) {
+        nextSteep.y = 0;
+        isChanged = true;
+    }
+    if (nextSteep.y < 0) {
+        nextSteep.y = GAME_SETTING.HEIGHT - GAME_SETTING.CELL_WIDTH;
+        isChanged = true;
+    }
+    return { isChanged, value: nextSteep };
 }
 
 function spawnFood(ctx) {
-  let isFoodOnSnake;
-  do {
-    // Tạo vị trí ngẫu nhiên cho thức ăn
-    food.x = getRandomCell().x;
-    food.y = getRandomCell().y;
-    // Mặc định không có thức ăn trên rắn
-    isFoodOnSnake = false;
-    // Kiểm tra xem vị trí của thức ăn mới có trùng với bất kỳ phần tử nào của rắn không
-    for (let i = 0; i < snake.cells.length; i++) {
-      const cell = snake.cells[i];
-      if (cell.x === food.x && cell.y === food.y) {
-        isFoodOnSnake = true;
-        break;
-      }
+    if (snake.cells.length + 5 == GAME_SETTING.VERTI_CELL_NUM * GAME_SETTING.HORI_CELL_NUM) {
+        resetGame();
     }
-  } while (isFoodOnSnake);
+    let isFoodOnSnake;
+    do {
+        // Tạo vị trí ngẫu nhiên cho thức ăn
+        food.x = getRandomCell().x;
+        food.y = getRandomCell().y;
+        // Mặc định không có thức ăn trên rắn
+        isFoodOnSnake = false;
+        // Kiểm tra xem vị trí của thức ăn mới có trùng với bất kỳ phần tử nào của rắn không
+        for (let i = 0; i < snake.cells.length; i++) {
+            const cell = snake.cells[i];
+            if (cell.x === food.x && cell.y === food.y) {
+                isFoodOnSnake = true;
+                break;
+            }
+        }
+    } while (isFoodOnSnake);
 
-  food.draw(ctx);
+    food.log(`food`)
+    food.draw(ctx);
 }
 
 function getRandomCell() {
-  const x =
-    Math.floor(Math.random() * GAME_SETTING.VERTI_CELL_NUM) *
-    GAME_SETTING.CELL_WIDTH;
-  const y =
-    Math.floor(Math.random() * GAME_SETTING.HORI_CELL_NUM) *
-    GAME_SETTING.CELL_WIDTH;
-  return { x, y };
+    const x =
+        Math.floor(Math.random() * GAME_SETTING.VERTI_CELL_NUM) *
+        GAME_SETTING.CELL_WIDTH;
+    const y =
+        Math.floor(Math.random() * GAME_SETTING.HORI_CELL_NUM) *
+        GAME_SETTING.CELL_WIDTH;
+    return { x, y };
+}
+
+function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0'); // Lấy giờ và thêm số 0 ở đầu nếu cần
+    const minutes = String(now.getMinutes()).padStart(2, '0'); // Lấy phút và thêm số 0 ở đầu nếu cần
+    const seconds = String(now.getSeconds()).padStart(2, '0'); // Lấy giây và thêm số 0 ở đầu nếu cần
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function renderHistoryList() {
+    let content = ``;
+    history.forEach((item) => {
+        console.log(item);
+        content += `<li>[${item.time}] Score: ${item.score}</li>`
+    })
+
+    scoreHistory.innerHTML = content;
 }
